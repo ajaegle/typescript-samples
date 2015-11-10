0b0511b0b7afe065d1572f88695a993c522fc499
ajaegle <mail@ajaegle.de>
2015-11-10 18:42:16 +0100

    add some more bugs (and a notice)

--- samples/01_intro/motivation.js
+++ samples/01_intro/motivation.js
@@ -1,3 +1,5 @@
+// this code intentionally has some bugs!
+
 function calcAggregate(data, extr, agg, initial) {
   data.reduce(function(prev, item) {
     return agg(prev, extr(item))
@@ -11,8 +13,9 @@ function extractItemValue(item) {
 function add(a, b) { return a + b }
 function multiply(a, b) { return a * b }
 
-//data = [{val:1}, {val:2}, {val:3}, {val:4}];
 data = [1,2,3,4].map(function (i) {return {val:i}});
 
 console.log(calcAggregate(data, extractItemValue, add, 0));
 console.log(calcAggregate(data, extractItemValue, multiply, 1));
+
+console.log(calcAggregate(data, add, extractItemValue, 0));
