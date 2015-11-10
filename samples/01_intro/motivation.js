3ab66d57fb67ede370c39d0dd4af954c4f642946
ajaegle <mail@ajaegle.de>
2015-11-10 18:51:04 +0100

    add one more error - missing parameter

--- samples/01_intro/motivation.js
+++ samples/01_intro/motivation.js
@@ -12,7 +12,7 @@ function multiply(a, b) { return a * b }
 
 data = [1,2,3,4].map(function (i) {return {val:i}});
 
+console.log(calcAggregate(data, extractItemValue, add));
 console.log(calcAggregate(data, extractItemValue, add, 0));
 console.log(calcAggregate(data, extractItemValue, multiply, 1));
-
 console.log(calcAggregate(data, add, extractItemValue, 0));
