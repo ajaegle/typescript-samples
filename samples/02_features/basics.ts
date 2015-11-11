de38f1d56c07b49bb9e4bf8699811929ea4d013f
ajaegle <mail@ajaegle.de>
2015-11-11 22:57:10 +0100

    add basic samples for typescript primitive types

---
+++ samples/02_features/basics.ts
@@ -0,0 +1,18 @@
+namespace basics {
+  var age: number;
+  var name: string;
+  var vip: boolean;
+  var emails: string[];
+}
+
+namespace basics_inferred {
+  var age = 52;
+  var name = "John Doe";
+  var vip = false;
+  var emails = ["john@doe.com"];
+
+  var unsure: any = 1337;
+  unsure = "i'm a string";
+
+  //console.log("", age, name, vip, emails, unsure)
+}
