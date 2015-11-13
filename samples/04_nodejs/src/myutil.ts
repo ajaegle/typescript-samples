075da45e4dd953a583671d4fbb20f38a8ff43402
ajaegle <mail@ajaegle.de>
2015-11-13 23:53:27 +0100

    switch to regular function

--- samples/04_nodejs/src/myutil.ts
+++ samples/04_nodejs/src/myutil.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 
-export var printFileContent = (fileName: string) => {
+export function printFileContent(fileName: string) {
+  "use strict";
   let filePath = `data/${fileName}`;
   fs.readFile(filePath, "utf-8", (err: NodeJS.ErrnoException, data: string) => {
     if (err) {
